9e72c2935836732b7f11f0fd343aaa61c44638aa
RyanPWebb2000 <Ryan.P.Webb-1@ou.edu>
2020-11-04 17:22:58 -0600

    Demonstrate the difference between HashMap and TreeMap

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeMap;
 
 
 public class Driver {
@@ -22,6 +25,33 @@ public class Driver {
 		
 		//Print the unique elements
 		System.out.println(set);
+		
+		
+		//Create a HashMap containing the same elements as list (Take note of the insertion order)
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		
+		hm.put(5, 5);
+		hm.put(4, 4);
+		hm.put(100000000, 7);
+		hm.put(10, 10);
+		hm.put(111, 500);
+		hm.put(11, 600);
+		
+		//Print to demonstrate that the order is not guaranteed and it is not sorted
+		System.out.println(hm);
+		
+		//Create a TreeMap with the same values as the HashMap
+		TreeMap<Integer, Integer> tm = new TreeMap<>();
+		
+		tm.put(5, 5);
+		tm.put(4, 4);
+		tm.put(100000000, 7);
+		tm.put(10, 10);
+		tm.put(111, 500);
+		tm.put(11, 600);
+		
+		//Print to demonstrate the the order is guaranteed to be sorted
+		System.out.println(tm);
 	}
 
 }
