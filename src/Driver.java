3c39cb2fc85c31157ea703de201db58b0247d808
RyanPWebb2000 <Ryan.P.Webb-1@ou.edu>
2020-11-04 16:58:32 -0600

    Demonstrate using set to find unique elements in a list

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Create a list containing duplicates
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(2);
+		list.add(3);
+		
+		//Create a set from that list. Since sets cannot have duplicate values it only returns the unique elements of the original list.
+		HashSet<Integer> set = new HashSet<Integer>();
+		for(Integer i : list) {
+			set.add(i);
+		}
+		
+		//Print the unique elements
+		System.out.println(set);
 	}
 
 }
